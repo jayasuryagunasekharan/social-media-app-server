829e835018678c8f916cd922edd4da83a809e05a
ashwinigit123 <ashwinitrale8@gmail.com>
2024-09-21 15:53:55 -0500

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB: 'mongodb+srv://jayasuryaguna:sC9XJPefHwTPOjZR@cluster0.bfa5h.mongodb.net/social-media-app?retryWrites=true&w=majority&appName=Cluster0',
+  SECRET_KEY: 'some very secret key'
+};
